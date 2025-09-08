ed810b117b22e64520ead6fd82b3490d81114517
Pratyasha Priyadarshini <priyadarshinipratyasha7@gmail.com>
2025-09-08 18:35:58 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import morgan from 'morgan';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js';
+import projectRoutes from './routes/project.routes.js';
+import aiRoutes from './routes/ai.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+
+connect();
+
+const app = express();
+const _dirname = path.resolve();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// serve frontend build
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
+app.use("/ai", aiRoutes);
+
+// fallback for SPA (React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
+
+export default app;
